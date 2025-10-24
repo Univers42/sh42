e7ce5a533ca7acc2f349d0517ed391ac1b63494e
LESdylan <dev.pro.photo@gmail.com>
2025-10-24 15:13:44 +0200

    update

--- srcs/sh.c
+++ srcs/sh.c
@@ -6,7 +6,7 @@
 /*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 18:43:35 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/10/24 15:12:22 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/10/24 15:13:38 by dlesieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int main(void)
 			rl_redisplay();
 		free(line);
 	}
+	printf("clear history\n");
 	printf("clear history\n");
 	rl_clear_history();
 	return (0);
