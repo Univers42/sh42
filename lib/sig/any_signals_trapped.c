cb276d5942155883dd741872400de5661ddc15ef
Dylan Lesieur <dlesieur@c1r2s3.42madrid.com>
2025-11-08 06:44:49 +0100

    fix(libsig): just fixed the any_signals_trapped for norminette compliancy

--- lib/sig/any_signals_trapped.c
+++ lib/sig/any_signals_trapped.c
@@ -6,7 +6,7 @@
 /*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 00:51:46 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/11/08 05:31:07 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/11/08 06:43:54 by dlesieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 // return >  0 if any of the real signals (not fake siganl like EXIT)
 // are trapped
-int	any_signals_trapped (void)
+int	any_signals_trapped(void)
 {
 	register int	i;
 
 	i = 0;
 	while (++i < NSIG)
-	if ((g_sig.sigmodes[i] & SIG_TRAPPED)
-		&& (g_sig.sigmodes[i] & SIG_IGNORED) == 0)
-		return (i);
-			return (-1);
-} 
+		if ((g_sig.sigmodes[i] & SIG_TRAPPED)
+			&& (g_sig.sigmodes[i] & SIG_IGNORED) == 0)
+			return (i);
+	return (-1);
+}
