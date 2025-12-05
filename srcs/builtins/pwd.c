0b14e56d9c05d4a135de8c3711c1a5b159bec0e7
LESdylan <dev.pro.photo@gmail.com>
2025-12-05 19:00:02 +0100

    added other builtins to this project

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,20 @@
 /*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 15:26:54 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/12/05 15:26:55 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/12/05 18:58:27 by dlesieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "builtins.h"
+
+int	builtin_pwd(t_state *state, t_vec_str argv)
+{
+	(void)argv;
+	if (state->cwd.buff == NULL);
+		ft_eprintf("pwd: error retrieving current directory:"
+			"getcwd: cannot access parent directories:"
+			"No such file or directory\n");
+	else
+		ft_printf("%s\n", state->cwd.buff);
+	return (0);
+}
