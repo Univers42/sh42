1314b280620ad550cb66a047354d679bf579a725
Dylan Lesieur <dlesieur@c1r13s5.42madrid.com>
2026-01-12 17:22:15 +0100

    the program is more and more compliant to posixx

--- bonus/src/word_splitting/reparse_envar_plain.c
+++ bonus/src/word_splitting/reparse_envar_plain.c
@@ -35,38 +35,3 @@ void	reparse_dq_bs(t_ast_node *ret, int *i, t_token t)
 	}
 	(*i)++;
 }
-
-void	reparse_dquote(t_ast_node *ret, int *i, t_token t)
-{
-	int		prev_start;
-	bool	special;
-
-	ft_assert(t.start[(*i)++] == '"');
-	prev_start = *i;
-	while (*i < t.len && t.start[*i] != '"')
-	{
-		special = t.start[*i] == '\\' || t.start[*i] == '$';
-		if (special)
-		{
-			t_ast_node tmp = create_subtoken_node(t, prev_start, *i, TT_DQWORD);
-			tmp.children.elem_size = sizeof(t_ast_node);
-			vec_push(&ret->children, &tmp);
-		}
-		if (t.start[*i] == '\\')
-			reparse_dq_bs(ret, i, t);
-		else if (t.start[*i] == '$')
-			reparse_envvar(ret, i, t, TT_DQENVVAR);
-		if (special)
-		{
-			prev_start = *i;
-			continue ;
-		}
-		(*i)++;
-	}
-	{
-		t_ast_node tmp = create_subtoken_node(t, prev_start, *i, TT_DQWORD);
-		tmp.children.elem_size = sizeof(t_ast_node);
-		vec_push(&ret->children, &tmp);
-	}
-	ft_assert(t.start[(*i)++] == '"');
-}
--- bonus/src/word_splitting/reparse_quote.c
+++ bonus/src/word_splitting/reparse_quote.c
@@ -98,64 +98,77 @@ void	reparse_envvar(t_ast_node *ret, int *i, t_token t, t_tt tt)
 	}
 }
 
-// void reparse_dquote(t_ast_node *ret, int *i, t_token t)
-// {
-// 	int	prev_start;
-// 	bool	special;
+void reparse_dquote(t_ast_node *ret, int *i, t_token t)
+{
+	int prev_start;
+	bool special;
 
-// 	ft_assert(t.start[(*i)++] == '"');
-// 	prev_start = *i;
-// 	while (*i < t.len && t.start[*i] != '"')
-// 	{
-// 		special = t.start[*i] == '\\' || t.start[*i] == '$';
-// 		if (special)
-// 		{
-// 			t_ast_node tmp = create_subtoken_node(t, prev_start, *i, TT_DQWORD);
-// 			tmp.children.elem_size = sizeof(t_ast_node);
-// 			vec_push(&ret->children, &tmp);
-// 		}
-// 		if (t.start[*i] == '\\')
-// 		{
-// 			/* handle backslash inside double quotes inline */
-// 			ft_assert(t.start[*i] == '\\');
-// 			(*i)++;
-// 			if (*i < t.len)
-// 			{
-// 				if (ft_strchr("\"$\\", t.start[*i]))
-// 				{
-// 					t_ast_node tmp2 = create_subtoken_node(t, *i, *i + 1, TT_DQWORD);
-// 					tmp2.children.elem_size = sizeof(t_ast_node);
-// 					vec_push(&ret->children, &tmp2);
-// 				}
-// 				else
-// 				{
-// 					t_ast_node tmp2 = create_subtoken_node(t, *i - 1, *i + 1, TT_DQWORD);
-// 					tmp2.children.elem_size = sizeof(t_ast_node);
-// 					vec_push(&ret->children, &tmp2);
-// 				}
-// 				(*i)++;
-// 			}
-// 			else
-// 			{
-// 				/* lone backslash at end -> treat as single-char DQ fragment */
-// 				t_ast_node tmp2 = create_subtoken_node(t, *i - 1, *i, TT_DQWORD);
-// 				tmp2.children.elem_size = sizeof(t_ast_node);
-// 				vec_push(&ret->children, &tmp2);
-// 			}
-// 		}
-// 		else if (t.start[*i] == '$')
-// 			reparse_envvar(ret, i, t, TT_DQENVVAR);
-// 		if (special)
-// 		{
-// 			prev_start = *i;
-// 			continue ;
-// 		}
-// 		(*i)++;
-// 	}
-// 	{
-// 		t_ast_node tmp = create_subtoken_node(t, prev_start, *i, TT_DQWORD);
-// 		tmp.children.elem_size = sizeof(t_ast_node);
-// 		vec_push(&ret->children, &tmp);
-// 	}
-// 	ft_assert(t.start[(*i)++] == '"');
-// }
+	ft_assert(t.start[(*i)++] == '"');
+	prev_start = *i;
+	while (*i < t.len && t.start[*i] != '"')
+	{
+		special = (t.start[*i] == '\\' || t.start[*i] == '$');
+		if (special)
+		{
+			/* push chunk before special char */
+			t_ast_node tmp = create_subtoken_node(t, prev_start, *i, TT_DQWORD);
+			tmp.children.elem_size = sizeof(t_ast_node);
+			vec_push(&ret->children, &tmp);
+		}
+		if (t.start[*i] == '\\')
+		{
+			/* handle backslash in double quotes: remove backslash only when
+			   followed by ", $, \\ or newline; otherwise keep the backslash */
+			int esc_pos = *i;
+			(*i)++;
+			if (*i < t.len)
+			{
+				char c = t.start[*i];
+				if (c == '\n')
+				{
+					/* backslash-newline: line continuation -> consume both, emit nothing */
+					/* nothing to push */
+				}
+				else if (c == '"' || c == '$' || c == '\\')
+				{
+					/* emit the escaped char without backslash */
+					t_ast_node tmp2 = create_subtoken_node(t, *i, *i + 1, TT_DQWORD);
+					tmp2.children.elem_size = sizeof(t_ast_node);
+					vec_push(&ret->children, &tmp2);
+				}
+				else
+				{
+					/* keep backslash as literal together with next char */
+					t_ast_node tmp2 = create_subtoken_node(t, esc_pos, *i + 1, TT_DQWORD);
+					tmp2.children.elem_size = sizeof(t_ast_node);
+					vec_push(&ret->children, &tmp2);
+				}
+				(*i)++;
+			}
+			else
+			{
+				/* backslash at end: include it as literal */
+				t_ast_node tmp2 = create_subtoken_node(t, esc_pos, *i, TT_DQWORD);
+				tmp2.children.elem_size = sizeof(t_ast_node);
+				vec_push(&ret->children, &tmp2);
+			}
+		}
+		else if (t.start[*i] == '$')
+		{
+			reparse_envvar(ret, i, t, TT_DQENVVAR);
+		}
+		if (special)
+		{
+			prev_start = *i;
+			continue;
+		}
+		(*i)++;
+	}
+	/* push remaining chunk inside quotes */
+	{
+		t_ast_node tmp = create_subtoken_node(t, prev_start, *i, TT_DQWORD);
+		tmp.children.elem_size = sizeof(t_ast_node);
+		vec_push(&ret->children, &tmp);
+	}
+	ft_assert(t.start[(*i)++] == '"');
+}
