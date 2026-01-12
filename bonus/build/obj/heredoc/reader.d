0e95ee0f9d65dafce88cdd5bd3f239df494a6897
Dylan Lesieur <dlesieur@c2r7s5.42madrid.com>
2026-01-12 03:51:39 +0100

    update

---
+++ bonus/build/obj/heredoc/reader.d
@@ -0,0 +1,113 @@
+build/obj/heredoc/reader.o: src/heredoc/reader.c \
+ vendor/libft/../libft/libft.h vendor/libft/include/ds.h \
+ vendor/libft/include/../srcs/ds/lists/ft_list.h \
+ vendor/libft/include/../srcs/ds/doubly_linked_list/ft_doubly_list.h \
+ vendor/libft/include/../srcs/ds/circular_linked_list/ft_circular_list.h \
+ vendor/libft/include/../srcs/ds/queue/ft_queue.h \
+ vendor/libft/include/ft_memory.h vendor/libft/include/ft_stddef.h \
+ vendor/libft/include/../srcs/memory/memalloc/lifoba/lifoba.h \
+ vendor/libft/include/../srcs/memory/memalloc/pool/pool.h \
+ vendor/libft/include/../srcs/memory/memalloc/slab/slab.h \
+ vendor/libft/include/../srcs/memory/memalloc/arena/arena.h \
+ vendor/libft/include/ft_debug.h vendor/libft/include/ft_math.h \
+ vendor/libft/include/../srcs/math/algebra/algebra.h \
+ vendor/libft/include/../srcs/ds/vector/ft_vector.h \
+ vendor/libft/include/ft_stddef.h \
+ vendor/libft/include/../srcs/ds/array/array.h \
+ vendor/libft/include/../srcs/ds/hash/hash.h \
+ vendor/libft/include/../srcs/ds/bitmap.h \
+ vendor/libft/include/../srcs/ds/dyn_string/dyn_string.h \
+ vendor/libft/include/ft_stdlib.h vendor/libft/include/ft_memory.h \
+ vendor/libft/include/../srcs/std/strto/strto.h \
+ vendor/libft/include/../srcs/std/prng.h vendor/libft/include/ft_string.h \
+ vendor/libft/include/ds.h vendor/libft/include/ft_ctype.h \
+ vendor/libft/include/ft_colors.h vendor/libft/include/ft_render.h \
+ vendor/libft/include/ft_colors.h vendor/libft/include/ft_sort.h \
+ vendor/libft/include/ft_debug.h vendor/libft/include/ft_stdio.h \
+ vendor/libft/include/../srcs/stdio/output/output.h \
+ vendor/libft/include/../srcs/stdio/output/error.h \
+ vendor/libft/include/../srcs/stdio/output/error.h \
+ vendor/libft/include/../srcs/stdio/input/input.h \
+ vendor/libft/include/../srcs/stdio/input/get_next_line.h \
+ vendor/libft/include/ft_stdio.h \
+ vendor/libft/include/../srcs/stdio/format/format.h \
+ vendor/libft/include/../srcs/stdio/format/../output/output.h \
+ vendor/libft/include/ft_math.h vendor/libft/include/ft_time.h \
+ vendor/libft/include/ft_wctype.h vendor/libft/include/ipc.h \
+ vendor/libft/include/../srcs/ipc/signals/trap.h \
+ vendor/libft/include/../srcs/ipc/pipes/pipe.h \
+ vendor/libft/include/../srcs/ipc/signals/quit.h \
+ vendor/libft/include/../srcs/ipc/signals/trap.h \
+ vendor/libft/include/system.h vendor/libft/include/ft_wctype.h \
+ incs/shell.h incs/alias.h vendor/libft/libft.h incs/public/ft_printf.h \
+ incs/public/signals.h incs/public/error.h incs/ast.h incs/public/token.h \
+ incs/executor.h incs/public/executor_types.h incs/redir.h incs/history.h \
+ incs/shell.h incs/prompt.h incs/redir.h incs/helpers.h
+vendor/libft/../libft/libft.h:
+vendor/libft/include/ds.h:
+vendor/libft/include/../srcs/ds/lists/ft_list.h:
+vendor/libft/include/../srcs/ds/doubly_linked_list/ft_doubly_list.h:
+vendor/libft/include/../srcs/ds/circular_linked_list/ft_circular_list.h:
+vendor/libft/include/../srcs/ds/queue/ft_queue.h:
+vendor/libft/include/ft_memory.h:
+vendor/libft/include/ft_stddef.h:
+vendor/libft/include/../srcs/memory/memalloc/lifoba/lifoba.h:
+vendor/libft/include/../srcs/memory/memalloc/pool/pool.h:
+vendor/libft/include/../srcs/memory/memalloc/slab/slab.h:
+vendor/libft/include/../srcs/memory/memalloc/arena/arena.h:
+vendor/libft/include/ft_debug.h:
+vendor/libft/include/ft_math.h:
+vendor/libft/include/../srcs/math/algebra/algebra.h:
+vendor/libft/include/../srcs/ds/vector/ft_vector.h:
+vendor/libft/include/ft_stddef.h:
+vendor/libft/include/../srcs/ds/array/array.h:
+vendor/libft/include/../srcs/ds/hash/hash.h:
+vendor/libft/include/../srcs/ds/bitmap.h:
+vendor/libft/include/../srcs/ds/dyn_string/dyn_string.h:
+vendor/libft/include/ft_stdlib.h:
+vendor/libft/include/ft_memory.h:
+vendor/libft/include/../srcs/std/strto/strto.h:
+vendor/libft/include/../srcs/std/prng.h:
+vendor/libft/include/ft_string.h:
+vendor/libft/include/ds.h:
+vendor/libft/include/ft_ctype.h:
+vendor/libft/include/ft_colors.h:
+vendor/libft/include/ft_render.h:
+vendor/libft/include/ft_colors.h:
+vendor/libft/include/ft_sort.h:
+vendor/libft/include/ft_debug.h:
+vendor/libft/include/ft_stdio.h:
+vendor/libft/include/../srcs/stdio/output/output.h:
+vendor/libft/include/../srcs/stdio/output/error.h:
+vendor/libft/include/../srcs/stdio/output/error.h:
+vendor/libft/include/../srcs/stdio/input/input.h:
+vendor/libft/include/../srcs/stdio/input/get_next_line.h:
+vendor/libft/include/ft_stdio.h:
+vendor/libft/include/../srcs/stdio/format/format.h:
+vendor/libft/include/../srcs/stdio/format/../output/output.h:
+vendor/libft/include/ft_math.h:
+vendor/libft/include/ft_time.h:
+vendor/libft/include/ft_wctype.h:
+vendor/libft/include/ipc.h:
+vendor/libft/include/../srcs/ipc/signals/trap.h:
+vendor/libft/include/../srcs/ipc/pipes/pipe.h:
+vendor/libft/include/../srcs/ipc/signals/quit.h:
+vendor/libft/include/../srcs/ipc/signals/trap.h:
+vendor/libft/include/system.h:
+vendor/libft/include/ft_wctype.h:
+incs/shell.h:
+incs/alias.h:
+vendor/libft/libft.h:
+incs/public/ft_printf.h:
+incs/public/signals.h:
+incs/public/error.h:
+incs/ast.h:
+incs/public/token.h:
+incs/executor.h:
+incs/public/executor_types.h:
+incs/redir.h:
+incs/history.h:
+incs/shell.h:
+incs/prompt.h:
+incs/redir.h:
+incs/helpers.h:
