59c16192e27e8cd7583d954b348fef77fba71037
alcacere <alcacere@localhost.com>
2026-01-27 19:29:23 +0100

    hotfix expander to avoid segfault in assignment_word_to_word

--- src/expander/assignment_word_to_word.c
+++ src/expander/assignment_word_to_word.c
@@ -6,10 +6,9 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 19:26:49 by marvin            #+#    #+#             */
-/*   Updated: 2026/01/22 19:26:49 by marvin           ###   ########.fr       */
+/*   Updated: 2026/01/27 18:58:57 by alcacere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "expander_private.h"
 
 void	assignment_word_to_word(t_ast_node *node)
@@ -17,17 +16,26 @@ void	assignment_word_to_word(t_ast_node *node)
 	t_ast_node	ret;
 	t_ast_node	left;
 	t_ast_node	right;
+	size_t		i;
 
 	ret = (t_ast_node){.node_type = AST_WORD};
 	vec_init(&ret.children);
 	ret.children.elem_size = sizeof(t_ast_node);
-	ft_assert(node->node_type == AST_ASSIGNMENT_WORD);
-	ft_assert(node->children.len == 2);
+	(ft_assert(node->node_type == AST_ASSIGNMENT_WORD), ft_assert(node->children.len == 2));
 	left = ((t_ast_node *)node->children.ctx)[0];
 	right = ((t_ast_node *)node->children.ctx)[1];
 	left.token.len++;
 	vec_push(&ret.children, &left);
-	vec_push(&ret.children, &right);
+	if (right.node_type == AST_WORD)
+	{
+		i = -1;
+		while (++i < right.children.len)
+			vec_push(&ret.children, vec_idx(&right.children, i));
+		free(right.children.ctx);
+	}
+	else
+		vec_push(&ret.children, &right);
 	free(node->children.ctx);
 	*node = ret;
 }
+
